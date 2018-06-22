ab593f84a7d070bcf5bc4927cf751cdf966b0812
Amo DelBello <amodelbello@gmail.com>
2018-06-22 12:31:59 -0600

    Adds materialize.css and a Navbar coponent

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Nav msg="Hello"></Nav>
     <img src="./assets/logo.png">
     <router-view/>
     <!-- <Login msg="Welcome to Your Vue.js App"/> -->
@@ -7,12 +8,14 @@
 </template>
 
 <script>
+import Nav from '@/components/authentication/Nav.vue'
 import Login from '@/components/authentication/Login.vue'
 import Register from '@/components/authentication/Register.vue'
 
 export default {
   name: 'app',
   components: {
+    Nav,
     Login,
     Register,
   }
@@ -26,6 +29,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+nav {
+  text-align: left;
+  padding: 0 5%;
 }
 </style>
---
+++ src/components/authentication/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Red or Green Admin - {{ msg }}</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="/">Login</a></li>
+        <li><a href="/register">Register</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
