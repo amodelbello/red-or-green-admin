40923844707c89d9505160e46671ffb24bd85e7f
Amo DelBello <amodelbello@gmail.com>
2018-07-09 10:00:49 -0600

    Improves authentication mixin logic and adds ability to get logged in user role

--- src/mixins/authentication.js
+++ src/mixins/authentication.js
@@ -1,26 +1,47 @@
 import { store } from '../store/store'
 
+const getAuthToken = () => {
+  return store.getters.authToken;
+};
+
+const getTokenPayload = () => {
+    const token = getAuthToken();
+    if (token) {
+      const payload = JSON.parse(window.atob(token.split('.')[1]));
+      return payload;
+    } else {
+      return null;
+    }
+};
+
 export default {
 
   isLoggedIn() {
-    const token = this.getAuthToken();
+    const token = getAuthToken();
 
-    return (
-      token !== undefined && 
-      token !== null &&
-      token !== ''
-    );
+    if (token) {
+      const payload = getTokenPayload();
+      return payload.expiration > Date.now() / 1000;
+    } else {
+      return false;
+    }
   },
 
   getHttpAuthHeader() {
     const config = {
-      headers: {'Authorization': 'Bearer ' + this.getAuthToken() }
+      headers: { 'Authorization': 'Bearer ' + getAuthToken() }
     };
 
     return config;
   },
 
-  getAuthToken() {
-    return store.getters.authToken;
+  getUserRole() {
+    const payload = getTokenPayload();
+    if (payload !== null) {
+      const userRole = getTokenPayload().role;
+      return userRole;
+    } else {
+      return null;
+    }
   }
 }
